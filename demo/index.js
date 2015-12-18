b38c588b207c8a00c0011fe675e2a6f661fd00f2
Lucas Motta <mail@lucasmotta.com>
2015-12-18 15:13:44 +0000

    Added new colorful demo

--- demo/index.js
+++ demo/index.js
@@ -6,33 +6,63 @@ const App = React.createClass({
   render() {
     return (
       <StickyStackContext>
-        <p>
-          Lorem Ipsum
-        </p>
+
+        <div className="divider divider--short" />
+
         <StickyStackItem position={0}>
-          <h1>
-            Live
-          </h1>
+          <div className="content">
+            <div className="logo">
+              <img className="logo__img" src="sticky-stack.svg" />
+            </div>
+          </div>
         </StickyStackItem>
-        <p>
-          Lorem Ipsum
-        </p>
+
+        <div className="divider" />
+
         <StickyStackItem position={1}>
-          <h2>
-            Your
-          </h2>
+          <div className="content">
+            <div className="block block--tall" />
+          </div>
         </StickyStackItem>
-        <p>
-          Lorem Ipsum
-        </p>
+
+        <div className="divider" />
+
         <StickyStackItem position={2}>
-          <h3>
-            City
-          </h3>
+          <div className="content">
+            <div className="block" />
+          </div>
         </StickyStackItem>
-        <p>
-          Lorem Ipsum
-        </p>
+
+        <div className="divider" />
+
+        <StickyStackItem position={3}>
+          <div className="content">
+            <div className="block block--short" />
+          </div>
+        </StickyStackItem>
+
+        <div className="divider" />
+
+        <StickyStackItem position={4}>
+          <div className="content">
+            <div className="block block--content">
+              <a className="link" href="https://github.com/YPlan/react-sticky-stack">Fork me on Github</a>
+            </div>
+          </div>
+        </StickyStackItem>
+
+        <div className="divider divider--short" />
+
+        <StickyStackItem position={5}>
+          <div className="content">
+            <div className="block block--credits">
+              <p>Made with ❤ by <a href="https://yplanapp.com">YPlan</a></p>
+            </div>
+          </div>
+        </StickyStackItem>
+
+        <div className="divider" />
+
       </StickyStackContext>
     );
   },
